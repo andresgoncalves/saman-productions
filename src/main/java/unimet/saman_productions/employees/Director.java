c07d37d0fdad756b8229d57ba718c99547900f44
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2024-02-05 19:25:29 -0400

    Added Director status

--- src/main/java/unimet/saman_productions/employees/Director.java
+++ src/main/java/unimet/saman_productions/employees/Director.java
@@ -6,6 +6,12 @@ public class Director extends Employee {
   public static final double CHECK_DURATION = (double) 35 / 60 / 24;
   public static final int SALARY = 60;
 
+  public static final int STATUS_IDLE = 0;
+  public static final int STATUS_PUBLISHING = 1;
+  public static final int STATUS_SUPERVISING = 2;
+
+  private int status = STATUS_IDLE;
+
   public Director(Studio studio) {
     super(studio);
   }
@@ -20,6 +26,7 @@ public class Director extends Employee {
     while (!isInterrupted()) {
       try {
         if (getStudio().getDeadlineCounter() == 0) {
+          status = STATUS_PUBLISHING;
           int standardEpisodeCount = getStudio().publishStandardEpisodes();
           int plotTwistEpisodeCount = getStudio().publishPlotTwistEpisodes();
           getStudio().registerEarning(
@@ -32,10 +39,12 @@ public class Director extends Employee {
 
           Thread.sleep(daysToMilliseconds(1));
         } else {
+          status = STATUS_IDLE;
           boolean isWatchingAnime = false;
           double randomCheckDelay = Math.random() * (1 - CHECK_DURATION);
           ProjectManager projectManager = getStudio().getEmployeeManager().getProjectManager();
 
+          status = STATUS_SUPERVISING;
           Thread.sleep(daysToMilliseconds(randomCheckDelay));
           if (projectManager.getStatus() == ProjectManager.STATUS_WATCHING_ANIME) {
             isWatchingAnime = true;
@@ -56,11 +65,16 @@ public class Director extends Employee {
             projectManager.registerFault();
           }
 
+          status = STATUS_IDLE;
           Thread.sleep(daysToMilliseconds(1 - randomCheckDelay + CHECK_DURATION));
         }
       } catch (InterruptedException e) {
         e.printStackTrace();
       }
     }
+  }
+
+  public int getStatus() {
+    return status;
   }
 }
